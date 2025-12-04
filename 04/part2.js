21e8a7d590da78f160f4170430b8975d062d5664
mrperrytpx <tomislav.siprak1996@gmail.com>
2025-12-04 16:21:07 +0100

    Advent of code 2025 day 4, bros have a lot of toilet paper

---
+++ 04/part2.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const arg = process.argv[2];
+
+let file = fs
+    .readFileSync(__dirname + `/${arg ? arg : "example"}.txt`, "utf-8")
+    .replace(/\r/g, "")
+    .split("\n");
+
+let mfd = [];
+for (let i = 0; i < file.length; i++) {
+    for (let j = 0; j < file[i].length; j++) {
+        if (file[i][j] === "@") mfd.push([i, j]);
+    }
+}
+
+let removed = new Set();
+while (mfd.length) {
+    let next = [];
+    for (let [i, j] of mfd) {
+        let n = 0;
+        for (let k = -1; k <= 1; k++) {
+            for (let m = -1; m <= 1; m++) {
+                if (k === 0 && m === 0) continue;
+                if (
+                    i + k >= file.length ||
+                    i + k < 0 ||
+                    j + m >= file[i].length ||
+                    j + m < 0
+                )
+                    continue;
+                const curr = file[i + k][j + m];
+
+                if (curr === "@" && !removed.has(`${i + k},${j + m}`)) n++;
+            }
+        }
+        if (n < 4) {
+            removed.add(`${i},${j}`);
+        } else {
+            next.push([i, j]);
+        }
+    }
+
+    if (mfd.length === next.length) {
+        console.log(removed.size);
+        break;
+    }
+
+    mfd = next;
+}
